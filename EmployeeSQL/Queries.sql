ad8078d590d71db09898174c35da935eac46a65b
Mario Lozano <Mario@m.lozano1733.com>
2021-01-19 06:26:31 -0500

    updated queries

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -7,3 +7,9 @@ INNER JOIN employees ON
 employees.emp_no=salaries.emp_no;
 
 "List first name, last name, and hire date for employees who were hired in 1986."
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date between '1986-01-01' and '1986-12-31'
+
+"List the manager of each department with the following information: department number, department name, 
+the manager's employee number, last name, first name."
