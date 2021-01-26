0d0d7eed8b3f9849de9f9176b5edc41f633ed708
Mario Lozano <Mario@m.lozano1733.com>
2021-01-26 01:25:17 -0500

    adjusted the order of the tables and PK/FK in order to import the data

--- EmployeeSQL/HP Employees.sql
+++ EmployeeSQL/HP Employees.sql
@@ -5,50 +5,67 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
-
-CREATE TABLE departments (
-	dept_no VARCHAR ,
-	dept_name VARCHAR,
-	PRIMARY KEY (dept_no)
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
-CREATE TABLE dept_emp (
-	emp_no VARCHAR,
-	dept_no VARCHAR,
-	PRIMARY KEY (emp_no),
-	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no VARCHAR,
-	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
-	FOREIGN KEY(emp_no) REFERENCES dept_emp (emp_no)
+CREATE TABLE "employees" (
+    "emp_no" VARCHAR   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" TIMESTAMP   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" TIMESTAMP   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
-CREATE TABLE employees (
-	emp_no VARCHAR 
-	emp_title_id VARCHAR,
-	birth_date TIMESTAMP,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date TIMESTAMP,
-	PRIMARY KEY (emp_title_id),
-	FOREIGN KEY(emp_no) REFERENCES dept_emp (emp_no)
+CREATE TABLE "dept_emp" (
+    "emp_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
 );
 
-CREATE TABLE salaries (
-	emp_no VARCHAR,
-	salary INT,
-	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL
 );
 
-CREATE TABLE titles (
-	title_id VARCHAR,
-	title VARCHAR,
-	FOREIGN KEY(title_id) REFERENCES employees (emp_title_id)
+CREATE TABLE "salaries" (
+    "emp_no" VARCHAR   NOT NULL,
+    "salary" INT   NOT NULL
 );
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 -- Import data from departments.csv, dept_emp.csv, dept_manager.csv, employees.csv, salaries.csv, and titles.csv.
 -- View the table to ensure all data has been imported correctly
