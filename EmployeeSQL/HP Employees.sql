68068745fe9c8eeeb6c9ed32e9089db53f1e0dfd
Mario Lozano <Mario@m.lozano1733.com>
2021-01-26 00:07:52 -0500

    started over to correctly assign primary keys and foreign keys prior to uploading data into database

--- EmployeeSQL/HP Employees.sql
+++ EmployeeSQL/HP Employees.sql
@@ -7,38 +7,47 @@ DROP TABLE IF EXISTS titles;
 
 
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
+	dept_no VARCHAR ,
+	dept_name VARCHAR,
+	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
-	emp_no SERIAL PRIMARY KEY,
-	dept_no VARCHAR FOREIGN KEY "FK >- departments.dept_no"
+	emp_no VARCHAR,
+	dept_no VARCHAR,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR FOREIGN KEY "FK >- departments.dept_no"
-	emp_no VARCHAR FOREIGN KEY "FK >- dept_emp.emp_no"
+	dept_no VARCHAR,
+	emp_no VARCHAR,
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+	FOREIGN KEY(emp_no) REFERENCES dept_emp (emp_no)
 );
 
 CREATE TABLE employees (
-	emp_no VARCHAR FOREIGN KEY "FK >- dept_emp.emp_no"
-	emp_title_id SERIAL PRIMARY KEY,
+	emp_no VARCHAR 
+	emp_title_id VARCHAR,
 	birth_date TIMESTAMP,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex VARCHAR,
-	hire_date TIMESTAMP
+	hire_date TIMESTAMP,
+	PRIMARY KEY (emp_title_id),
+	FOREIGN KEY(emp_no) REFERENCES dept_emp (emp_no)
 );
 
 CREATE TABLE salaries (
-	emp_no SERIAL FOREIGN KEY "FK >- dept_emp.emp_no"
-	salary INT
+	emp_no VARCHAR,
+	salary INT,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE titles (
 	title_id VARCHAR,
-	title VARCHAR
+	title VARCHAR,
+	FOREIGN KEY(title_id) REFERENCES employees (emp_title_id)
 );
 
 -- Import data from departments.csv, dept_emp.csv, dept_manager.csv, employees.csv, salaries.csv, and titles.csv.
