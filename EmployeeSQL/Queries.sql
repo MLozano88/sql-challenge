84788cac141c744910f08a7835cd26f527951eb4
Mario Lozano <Mario@m.lozano1733.com>
2021-01-26 02:19:16 -0500

    updated queries

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -12,21 +12,29 @@ FROM employees
 WHERE hire_date between '1986-01-01' and '1986-12-31'
 
 "List the manager of each department with the following information: department number, department name, 
-the manager's employee number, last name, first name.(INCOMPLETE!!!)"
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE emp_no IN
-(
-	SELECT emp_no
-	FROM dept_emp
-	WHERE emp_no IN
-	(
-		SELECT emp_no
-		FROM dept_manager
-		WHERE dept_no IN
-		(
-			SELECT dept_no
-			FROM departments
-		)
-	)
-);
+the manager's employee number, last name, first name."
+SELECT employees.emp_no, employees.first_name, employees.last_name,dept_manager.dept_no,departments.dept_name
+FROM dept_manager
+INNER JOIN employees ON
+employees.emp_no=dept_manager.emp_no
+LEFT JOIN departments ON
+dept_manager.dept_no = departments.dept_no;
+
+
+"List the department of each employee with the following information: 
+employee number, last name, first name, and department name."
+
+
+"List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B.""
+
+
+"List all employees in the Sales department, including their employee number,
+ last name, first name, and department name."
+
+
+"List all employees in the Sales and Development departments, 
+including their employee number, last name, first name, and department name."
+
+
+"In descending order, list the frequency count of employee last names, 
+i.e., how many employees share each last name."
