9f53d354e311781933e5f02d9b751a2f78b8d09f
Mario Lozano <Mario@m.lozano1733.com>
2021-01-27 23:32:39 -0500

    updated query

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -23,10 +23,18 @@ dept_manager.dept_no = departments.dept_no;
 
 "List the department of each employee with the following information: 
 employee number, last name, first name, and department name."
-
+SELECT employees.emp_no, employees.first_name, employees.last_name,departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no=departments.dept_no;
+ 
 
 "List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B.""
-
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name is "Hercules"
 
 "List all employees in the Sales department, including their employee number,
  last name, first name, and department name."
