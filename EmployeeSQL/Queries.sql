fcd9e22d5dec221becb9013415b81d8297894699
Mario Lozano <Mario@m.lozano1733.com>
2021-01-27 23:47:40 -0500

    updated query

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -49,7 +49,13 @@ WHERE dept_name LIKE 'Sales';
 
 "List all employees in the Sales and Development departments, 
 including their employee number, last name, first name, and department name."
-
+SELECT employees.emp_no, employees.last_name, employees.first_name,departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no=departments.dept_no
+WHERE dept_name LIKE 'Sales' OR dept_name LIKE 'Development';
 
 "In descending order, list the frequency count of employee last names, 
 i.e., how many employees share each last name."
