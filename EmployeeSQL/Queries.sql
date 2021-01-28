c5198f294c8b362d2e7aea1c4dd8f2a046729532
Mario Lozano <Mario@m.lozano1733.com>
2021-01-28 00:02:02 -0500

    final query

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -59,3 +59,7 @@ WHERE dept_name LIKE 'Sales' OR dept_name LIKE 'Development';
 
 "In descending order, list the frequency count of employee last names, 
 i.e., how many employees share each last name."
+SELECT last_name, COUNT(last_name) AS "frequency"
+FROM employees 
+GROUP BY last_name
+ORDER BY frequency DESC;
